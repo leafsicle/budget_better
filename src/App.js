a26362fec49852294a2f44d149399acd2f83188d
Matt Cooke <cookem529@gmail.com>
2018-10-30 15:39:52 -0400

    added bulma

--- src/App.js
+++ src/App.js
@@ -1,28 +1,33 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import logo from './logo.svg'
+import './App.css'
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+	render() {
+		return (
+			<div className="container">
+				<section class="hero">
+					<div class="hero-body">
+						<h1 class="title">Surp/Def</h1>
+						<h2 class="subtitle">Month</h2>
+					</div>
+				</section>
+				<div class="tabs is-small =mobile-only is-fullwidth">
+					<ul>
+						<li class="is-active">
+							<a>Overview</a>
+						</li>
+						<li>
+							<a>Monthly</a>
+						</li>
+						<li>
+							<a>Budget</a>
+						</li>
+					</ul>
+				</div>
+			</div>
+		)
+	}
 }
 
-export default App;
+export default App
