3b6cac2c8fa91eaa1bcb30a912c77be26b562c29
Matt Cooke <cookem529@gmail.com>
2018-11-02 13:45:01 -0400

    added moment to set the month real time

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import './App.css'
+import Moment from 'react-moment'
 
 class App extends Component {
 	render() {
@@ -24,25 +25,33 @@ class App extends Component {
 			<div className="container">
 				<section className="hero is-bold is-dark has-text-weight-semibold	">
 					<div className="hero-body title">Better Budget</div>
+					<h2 className="subtitle">
+						<Moment format="MMMM" />
+					</h2>
 				</section>
 				<main>
 					<div class="columns container is-gapless is-1-mobile">
 						<div class="column ">
 							<div class="box">
 								<h1 className="title has-text-success">$42</h1>
-								<h2 className="subtitle">October</h2>
+							</div>
+						</div>
+						<div class="column ">
+							<div class="box">
+								<h1 className="title has-text-danger">Upcoming bills</h1>
+								<h2 className="subtitle">3 Bills remaining this month</h2>
+							</div>
+						</div>
+						<div class="column">
+							<div class="box">
+								<h1 className="title has-text-info">$9,001</h1>
+								<h2 className="subtitle">It's over 9,000</h2>
 							</div>
 						</div>
 						<div class="column ">
 							<div class="box">
 								<h1 className="title has-text-danger">Upcoming bills</h1>
 								<h2 className="subtitle">5 Bills unpaid</h2>
-							</div>
-						</div>
-						<div class="column">
-							<div class="box">
-								<h1 className="title has-text-info">$9,001</h1>
-								<h2 className="subtitle">oiajsdojasd</h2>
 							</div>
 						</div>
 					</div>
