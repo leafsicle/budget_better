9e8b6f1f98cfbfc24eed138d619295918eb300c8
Matt Cooke <cookem529@gmail.com>
2018-11-03 17:47:35 -0400

    started to testing menus

--- src/Default.js
+++ src/Default.js
@@ -6,17 +6,18 @@ class Default extends Component {
 		return (
 			<section className="hero is-bold is-dark has-text-weight-semibold	">
 				<nav class="navbar" role="navigation" aria-label="main navigation">
-					<div class="navbar-brand">
-						<a class="active" aria-current="page" href="#/">
+					<div class="navbar-brand ">
+						<a class="active" aria-current="page" href="">
 							<i class="fas fa-balance-scale fa-3x" />
 						</a>
 						<div className="hero-body title">Better Budget</div>
+						<h2 className="subtitle">
+							<Moment element="span" format="MMMM" />
+						</h2>
 					</div>
 				</nav>
 
-				<h2 className="subtitle">
-					<Moment element="span" format="MMMM" />
-				</h2>
+				{/* <Moment element="span" format="MMMM" /> */}
 			</section>
 		)
 	}
