156caedab2725bf66cee72eb76de5639997cef1b
Matt Cooke <cookem529@gmail.com>
2018-11-03 22:30:44 -0400

    safety commit!

--- src/Test.jsx
+++ src/Test.jsx
@@ -27,20 +27,19 @@ class Test extends Component {
 			}
 		})
 		return (
-			<nav className="navbar" role="navigation" aria-label="main navigation">
-				<div className="navbar-brand">
-					<a className="navbar-item" href="https://bulma.io">
+			<nav class="navbar" role="navigation" aria-label="main navigation">
+				<div class="navbar-brand">
+					<a class="navbar-item" href="https://bulma.io">
 						<img
 							src="https://bulma.io/images/bulma-logo.png"
 							width="112"
 							height="28"
-							alt="This is a logo"
 						/>
 					</a>
 
 					<a
 						role="button"
-						className="navbar-burger burger"
+						class="navbar-burger burger"
 						aria-label="menu"
 						aria-expanded="false"
 						data-target="navbarBasicExample"
@@ -51,32 +50,32 @@ class Test extends Component {
 					</a>
 				</div>
 
-				<div id="navbarBasicExample" className="navbar-menu">
-					<div className="navbar-start">
-						<a className="navbar-item">Home</a>
+				<div id="navbarBasicExample" class="navbar-menu">
+					<div class="navbar-start">
+						<a class="navbar-item">Home</a>
 
-						<a className="navbar-item">Documentation</a>
+						<a class="navbar-item">Documentation</a>
 
-						<div className="navbar-item has-dropdown is-hoverable">
-							<a className="navbar-link">More</a>
+						<div class="navbar-item has-dropdown is-hoverable">
+							<a class="navbar-link">More</a>
 
-							<div className="navbar-dropdown">
-								<a className="navbar-item">About</a>
-								<a className="navbar-item">Jobs</a>
-								<a className="navbar-item">Contact</a>
-								<hr className="navbar-divider" />
-								<a className="navbar-item">Report an issue</a>
+							<div class="navbar-dropdown">
+								<a class="navbar-item">About</a>
+								<a class="navbar-item">Jobs</a>
+								<a class="navbar-item">Contact</a>
+								<hr class="navbar-divider" />
+								<a class="navbar-item">Report an issue</a>
 							</div>
 						</div>
 					</div>
 
-					<div className="navbar-end">
-						<div className="navbar-item">
-							<div className="buttons">
-								<a className="button is-primary">
+					<div class="navbar-end">
+						<div class="navbar-item">
+							<div class="buttons">
+								<a class="button is-primary">
 									<strong>Sign up</strong>
 								</a>
-								<a className="button is-light">Log in</a>
+								<a class="button is-light">Log in</a>
 							</div>
 						</div>
 					</div>
