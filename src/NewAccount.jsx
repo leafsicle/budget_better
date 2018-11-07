ed6cf6d9a4b2700eb02f7c37085c895cbb11307e
Matt Cooke <cookem529@gmail.com>
2018-11-06 20:28:47 -0500

    added input fields

--- src/NewAccount.jsx
+++ src/NewAccount.jsx
@@ -8,11 +8,11 @@ class NewAccount extends Component {
 				<PageHeader />
 				{/* This is where you will add a new account!! */}
 				<div class="field">
-					<label class="label">Label</label>
+					<label class="label">Account Name!</label>
 					<div class="control">
 						<input class="input" type="text" placeholder="Text input" />
 					</div>
-					<p class="help">This is a help text</p>
+					<p class="help">This is below the box?</p>
 				</div>
 				<div class="field">
 					<label class="label">Label</label>
