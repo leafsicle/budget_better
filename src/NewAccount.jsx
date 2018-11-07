24c1120702c872b0e23aefe995dfe745902d7f6d
Matt Cooke <cookem529@gmail.com>
2018-11-07 08:54:05 -0500

    Changing inputs

--- src/NewAccount.jsx
+++ src/NewAccount.jsx
@@ -4,29 +4,72 @@ import PageHeader from './PageHeader'
 class NewAccount extends Component {
 	render() {
 		return (
-			<div>
+			<div className="container">
 				<PageHeader />
-				{/* This is where you will add a new account!! */}
-				<div class="field">
-					<label class="label">Account Name!</label>
-					<div class="control">
-						<input class="input" type="text" placeholder="Text input" />
+				<div className="container new-account">
+					{/* This is where you will add a new account!! */}
+					<div class="field">
+						<label class="label">Account Name</label>
+						<div class="control">
+							<input
+								class="input"
+								type="text"
+								placeholder="What should we call this account?"
+							/>
+						</div>
+						<p class="help" /> {/* this appears below */}
 					</div>
-					<p class="help">This is below the box?</p>
-				</div>
-				<div class="field">
-					<label class="label">Amount Due</label>
-					<div class="control">
-						<input class="input" type="integer" placeholder="Amount Due" />
+					<div class="field">
+						<label class="label">Amount Due</label>
+						<div class="control">
+							<input
+								class="input"
+								type="number"
+								min="0"
+								max="100"
+								placeholder="Amount in $"
+							/>
+						</div>
+						<p class="help" /> {/* this appears below */}
 					</div>
-					<p class="help">Use whole numbers</p>
-				</div>
-				<div class="field">
-					<label class="label">Label</label>
-					<div class="control">
-						<input class="input" type="text" placeholder="Text input" />
+
+					<div class="field">
+						<label class="label">When is this bill due?</label>
+						<div class="control">
+							<input class="input" type="date" required placeholder="" />
+						</div>
+						<p class="help" /> {/* this appears below */}
 					</div>
-					<p class="help">This is a help text</p>
+					<div class="field container">
+						<label class="label">Frequency</label>
+						<div class="control">
+							<select id="frequency">
+								<option value="">How often does this occur?</option>
+								<option value="dog">Daily</option>
+								<option value="cat">Weekly</option>
+								<option value="hamster">Bi-weekly</option>
+								<option value="parrot">Monthly</option>
+								<option value="spider">Quarterly</option>
+								<option value="goldfish">Annually</option>
+							</select>
+						</div>
+						<p class="help" /> {/* this appears below */}
+					</div>
+
+					<div class="username">
+						<label for="uname">Notes:</label>
+						<input
+							type="text"
+							id="notes"
+							name="uname"
+							required
+							minlength="0"
+							maxlength="32"
+							placeholder="account number etc..."
+						/>
+						<span class="validity" />
+					</div>
+					<input className="" type="submit" value="Submit" />
 				</div>
 			</div>
 		)
--- src/Stack.js
+++ src/Stack.js
@@ -6,11 +6,11 @@ class Stack extends Component {
 		return (
 			<main>
 				<DefaultHeader />
-				<div class="columns container is-gapless is-1-mobile">
+				<div className="columns container is-gapless is-1-mobile">
 					{/* Overall card with working link */}
-					<div class="column">
+					<div className="column">
 						<a href="./">
-							<div class="box">
+							<div className="box">
 								<h1 className="title has-text-success">$42</h1>
 								<h2 className="subtitle">Surplus/Deficit this month</h2>
 							</div>
@@ -18,9 +18,9 @@ class Stack extends Component {
 					</div>
 
 					{/* Unpaid card with working link */}
-					<div class="column">
+					<div className="column">
 						<a href="./">
-							<div class="box">
+							<div className="box">
 								<h1 className="title has-text-danger">
 									### Bills remaining this month
 								</h1>{' '}
@@ -30,9 +30,9 @@ class Stack extends Component {
 					</div>
 
 					{/* Accounts card with working link */}
-					<div class="column">
+					<div className="column">
 						<a href="./Accounts">
-							<div class="box">
+							<div className="box">
 								<h1 className="title has-text-info">Accounts</h1>
 								<h2 className="subtitle">you have 10 account active</h2>
 							</div>
@@ -40,9 +40,9 @@ class Stack extends Component {
 					</div>
 
 					{/* Upcoming bills. Needs redirect */}
-					<div class="column">
+					<div className="column">
 						<a href="./">
-							<div class="box">
+							<div className="box">
 								<h1 className="title has-text-warning ">Upcoming bills</h1>
 								<h2 className="subtitle">5 Bills unpaid</h2>
 							</div>
